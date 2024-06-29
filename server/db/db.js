0f36ddde964fd858d902857d58e2abbcfb957b86
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2024-06-29 19:08:56 -0400

    dotenv

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,11 @@
 import { Pool } from "pg";
+import "dotenv/config";
 
 const pool = new Pool({
-  user: "siobhan",
-  host: "localhost",
-  database: "blog_example",
-  port: 5432,
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
